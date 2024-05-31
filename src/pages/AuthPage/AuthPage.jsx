5e7c6ad8395c6eda36891bc5c0041ad8bafb42c0
Mariana Lozynska <mariana.lozynska@imgarena.com>
2024-06-01 00:59:23 +0100

    feat: add authPage, pageLayout and SideBar

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,7 +2,7 @@ import { Flex, Container, Box, Image, VStack } from "@chakra-ui/react";
 import AuthForm from "../../components/AuthForm/AuthForm";
 const AuthPage = () => {
   return (
-    <Flex minH={"100vh"} judtifyContent={"center"} alignItems={"center"} px={4}>
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
       <Container maxW={"container.md"} padding={0}>
         <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
           {/* Left-hand-side */}
